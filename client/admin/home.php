2f3e08ac7a24809d7bd0284afece834dd78afad8
iam-migz <mareyes2132@gmail.com>
2021-05-18 10:18:57 +0800

    something

--- client/admin/home.php
+++ client/admin/home.php
@@ -14,8 +14,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 <body>
-    <?php include '../partials/header.php'; ?>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-sm-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">edit school info</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-sm-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">add school year</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- server/teachers_toolkit.sql
+++ server/teachers_toolkit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2021 at 05:33 AM
+-- Generation Time: May 18, 2021 at 04:18 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -43,8 +43,8 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `user_id`, `school_id`, `firstname`, `lastname`, `middlename`, `phone_no`, `email`) VALUES
-(21, 1029, 22, 'miguel', 'reyes', 'romuga', 2147483647, 'mr.reyes101lmfao@gmail.com'),
-(22, 1030, 23, 'sam', 'gamgee', 'wise', 12912, 'mreirj@iqjr');
+(23, 1033, 24, 'miguel', 'reyes', 'romuga', 2147483647, 'mareyes2132@gmail.com'),
+(24, 1042, 25, 'miguel', 'reyes', 'romuga', 2147483647, 'mr.reyes101lmfao@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -71,8 +71,143 @@ CREATE TABLE `schools` (
 --
 
 INSERT INTO `schools` (`id`, `address`, `barangay`, `city`, `province`, `country`, `postal_code`, `principal_fn`, `principal_ln`, `principal_mn`, `school_name`) VALUES
-(22, '', '', '', '', '', 0, '', '', '', ''),
-(23, '', '', '', '', '', 0, '', '', '', '');
+(24, '', '', '', '', '', 0, '', '', '', ''),
+(25, '', '', '', '', '', 0, '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school_records`
+--
+
+CREATE TABLE `school_records` (
+  `id` int(11) NOT NULL,
+  `section_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `school_year_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `school_records`
+--
+
+INSERT INTO `school_records` (`id`, `section_id`, `subject_id`, `teacher_id`, `school_year_id`) VALUES
+(1, 3, 2, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school_years`
+--
+
+CREATE TABLE `school_years` (
+  `id` int(11) NOT NULL,
+  `school_id` int(11) NOT NULL,
+  `sy_start` date NOT NULL,
+  `sy_end` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `school_years`
+--
+
+INSERT INTO `school_years` (`id`, `school_id`, `sy_start`, `sy_end`) VALUES
+(2, 25, '0000-00-00', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+
+CREATE TABLE `sections` (
+  `id` int(11) NOT NULL,
+  `advisor_id` int(11) DEFAULT NULL,
+  `section_name` varchar(100) NOT NULL,
+  `strand` varchar(100) NOT NULL,
+  `track` varchar(50) NOT NULL,
+  `grade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sections`
+--
+
+INSERT INTO `sections` (`id`, `advisor_id`, `section_name`, `strand`, `track`, `grade`) VALUES
+(3, 69, 'Newton', 'STEM', 'Academic', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(5) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `section_id` int(5) DEFAULT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `barangay` varchar(100) NOT NULL,
+  `gender` enum('m','f') NOT NULL,
+  `LRN` int(11) NOT NULL,
+  `birthdate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `user_id`, `section_id`, `firstname`, `lastname`, `middlename`, `email`, `province`, `city`, `barangay`, `gender`, `LRN`, `birthdate`) VALUES
+(2, 1041, NULL, 'firstname', 'lastname', 'middlename', 'email', 'province', 'city', 'barangay', '', 0, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `subject_name` varchar(100) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `hours` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subject_name`, `semester`, `hours`) VALUES
+(2, '', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `phone_no` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`id`, `user_id`, `firstname`, `lastname`, `middlename`, `phone_no`, `email`) VALUES
+(1, 1036, 'firstname', 'lastname', 'middlename', 92841042, 'example@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -91,8 +226,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `password`, `access`) VALUES
-(1029, '$2y$10$TU/phZKeVlwFncEAHCc5DejHHVAFhU5mQBScck0K.EPpChOrq.lwu', '3'),
-(1030, '$2y$10$Q0fStoAExZSiKyemGtU1FupThcAbMSSalD26LWSv83nPrM4r9Hih6', '3');
+(1033, '$2y$10$YUleb9YxgRLU0XCwh0MziOfwzzbJNkabbopvyYiVpy8OC/8iLGBVu', '3'),
+(1034, '$2y$10$DMjSwrdR6cwtBDovLDNguuPGUGXV91xoLfZtor8a88Sueo.Ymp3Nu', '2'),
+(1035, '$2y$10$Ydd.tNU7lJ3hxy6RzIGpWOVLLrrBHKCZttyhkZMhWkax8P8LOygFG', '2'),
+(1036, '$2y$10$ZRPFLWa2lMxWRAvk5GHULOzbyRX4aj/QJgIbaRtNxbGV4qJmfq5iO', '2'),
+(1037, '$2y$10$x7kJtCh6B4rPiWM1.FZIm.L2Etl10uhOVdCm9mxIzUfV0gBlTdBGS', '1'),
+(1038, '$2y$10$6i.a7ufv02X6u6dS0HoYn.dNOmzywtzKIUFx10RqpC0tjEUM/Hdma', '1'),
+(1039, '$2y$10$yoCr86ckylRTRE25lz0hI.ZCwENHcoER5X3WgNU8ZcDgcoAvFSe/2', '1'),
+(1040, '$2y$10$lAVGVpqWWvYyNo014ONavekDU7sAa8Cqdy7VQc9f0PTZLnixAUP7.', '1'),
+(1041, '$2y$10$uuxmmf7Chr8BPmyhioNDHu059qLBvqNf.23pyQ9WStqq4Hkvl2cZu', '1'),
+(1042, '$2y$10$ueBd.s5GIiRt.QsM4JAHc.QJWjsTXkKHSyzDdiCNlmWkwujb5dZw.', '3');
 
 --
 -- Indexes for dumped tables
@@ -113,6 +256,49 @@ ALTER TABLE `schools`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `school_records`
+--
+ALTER TABLE `school_records`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sr_section_fk` (`section_id`),
+  ADD KEY `sr_subject_fk` (`subject_id`),
+  ADD KEY `sr_teacher_fk` (`teacher_id`),
+  ADD KEY `sr_school_year_fk` (`school_year_id`);
+
+--
+-- Indexes for table `school_years`
+--
+ALTER TABLE `school_years`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `school_years_school_fk` (`school_id`);
+
+--
+-- Indexes for table `sections`
+--
+ALTER TABLE `sections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `students_user_fk` (`user_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacher_user_fk` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -126,19 +312,55 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `schools`
 --
 ALTER TABLE `schools`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `school_records`
+--
+ALTER TABLE `school_records`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `school_years`
+--
+ALTER TABLE `school_years`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `sections`
+--
+ALTER TABLE `sections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1031;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1043;
 
 --
 -- Constraints for dumped tables
@@ -150,6 +372,33 @@ ALTER TABLE `users`
 ALTER TABLE `admins`
   ADD CONSTRAINT `school_id_fk` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `school_records`
+--
+ALTER TABLE `school_records`
+  ADD CONSTRAINT `sr_school_year_fk` FOREIGN KEY (`school_year_id`) REFERENCES `school_years` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `sr_section_fk` FOREIGN KEY (`section_id`) REFERENCES `sections` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `sr_subject_fk` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `sr_teacher_fk` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `school_years`
+--
+ALTER TABLE `school_years`
+  ADD CONSTRAINT `school_years_school_fk` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD CONSTRAINT `teacher_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
