bcd26274a2d1f506b614410969b80402448c4204
Mark-Ameka <xenonmendozaxenon@gmail.com>
2021-05-19 22:18:24 +0800

    home adjusted

--- client/admin/home.php
+++ client/admin/home.php
@@ -14,32 +14,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .container{
+            margin-top: 4%;
+        }
+        .btn_proceed{
+            width: 97%;
+        }
+    </style>
 </head>
 <body>
     <?php include '../partials/header.php'; ?>
+    
     <div class="container">
-        <div class="row mt-4">
-            <div class="col-sm-6">
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">edit school info</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="row row-cols-1 row-cols-md-2">
+            <div class="col mb-5">
+                <!--Card Narrow-->
+                <div class="card card-cascade narrower">
+                    <div class="view view-cascade overlay">
+                        <img class="card-img-top" src="../images/school.jpg" alt="Card image cap">
+                        <a><div class="mask rgba-white-slight"></div></a>
+                    </div>
+                    <div class="card-body card-body-cascade">
+                        <h4 class="font-weight-bold card-title">Edit School Info</h4>
+                        <p class="card-text">Administrators of schools and districts can conveniently 
+                            edit their school or district profiles using the Admin Portal.</p>
+                        <a class="btn btn-dark-green btn_proceed">Proceed</a>
+                    </div>
                 </div>
-                </div>
+                <!--Card Narrow End-->
             </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-sm-6">
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">add school year</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="col mb-5">
+                <!--Card Narrow-->
+                <div class="card card-cascade narrower">
+                    <div class="view view-cascade overlay">
+                        <img class="card-img-top" src="../images/school2.jpg" alt="Card image cap">
+                        <a><div class="mask rgba-white-slight"></div></a>
+                    </div>
+                    <div class="card-body card-body-cascade">
+                        <h4 class="font-weight-bold card-title">Add School Year</h4>
+                        <p class="card-text">Adding days would be ineffective, and adding hours would be even more so. A systematic rethinking 
+                            of our school year philosophy could be beneficial.</p>
+                        <a class="btn btn-dark-green btn_proceed">Proceed</a>
+                    </div>
                 </div>
-                </div>
+                <!--Card Narrow End-->
             </div>
         </div>
     </div>
+
 </body>
 </html>
