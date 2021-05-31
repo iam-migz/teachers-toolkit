97985d3f0b0e8ae7066a02c9971bd1267aa2ea75
iam-migz <mareyes2132@gmail.com>
2021-05-31 10:39:07 +0800

    student grade fix

--- client/student/grades.php
+++ client/student/grades.php
@@ -103,12 +103,6 @@
                     <!-- insert data here -->
                 </tbody>
 
-
-
-
-
-
-
                 </table>
         </div>
     </div>
@@ -130,15 +124,15 @@
         let student_id = <?php echo $_SESSION['account_id']; ?>;
         document.addEventListener("DOMContentLoaded", async () => { 
 
-            let res, temp, tr, grade11 = [], grade12 = []
-            let q1_grade, q2_grade, q1_crd, q2_crd
             try {
-                res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord/read_student_grades.php?student_id=${student_id}`)
-                let student_data = res.data.data;
+
+                let res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/grades/student_grades.php?student_id=${student_id}`)
+                let student_data = res.data;
                 console.log('student_data :>> ', student_data);
 
                 // divide by school year id
-                temp = student_data[0].sy_id; 
+                let grade11 = [], grade12 = []
+                let temp = student_data[0].sy_id; 
                 student_data.forEach(data => {
                     if(data.sy_id == temp) {
                         grade11.push(data)
@@ -164,89 +158,18 @@
                 console.log('grade11_sem1 :>> ', grade11_sem1);
                 console.log('grade11_sem2 :>> ', grade11_sem2);
 
-                // insert grade 11 sem 1
-                let grade11_sem1_insert_to = document.querySelector("#grade11_sem1");
-                grade11_sem1.forEach(async(data, index) => {
 
-                    if (data.quarter == 2){
-                        return;
-                    }
-                    // get classrecord detail quarter 1
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=1`);
-                    q1_crd = res.data;
-                    q1_crd.total_highest_written = Number(q1_crd.hw1) + Number(q1_crd.hw2) + Number(q1_crd.hw3) + Number(q1_crd.hw4) + Number(q1_crd.hw5) + Number(q1_crd.hw6) + Number(q1_crd.hw7) + Number(q1_crd.hw8) + Number(q1_crd.hw9) + Number(q1_crd.hw10);
-                    q1_crd.total_highest_performance = Number(q1_crd.hp1) + Number(q1_crd.hp2) + Number(q1_crd.hp3) + Number(q1_crd.hp4) + Number(q1_crd.hp5) + Number(q1_crd.hp6) + Number(q1_crd.hp7) + Number(q1_crd.hp8) + Number(q1_crd.hp9) + Number(q1_crd.hp10);
-                    
-                    // get classrecord detail quarter 2
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=2`);
-                    q2_crd = res.data;
-                    q2_crd.total_highest_written = Number(q2_crd.hw1) + Number(q2_crd.hw2) + Number(q2_crd.hw3) + Number(q2_crd.hw4) + Number(q2_crd.hw5) + Number(q2_crd.hw6) + Number(q2_crd.hw7) + Number(q2_crd.hw8) + Number(q2_crd.hw9) + Number(q2_crd.hw10);
-                    q2_crd.total_highest_performance = Number(q2_crd.hp1) + Number(q2_crd.hp2) + Number(q2_crd.hp3) + Number(q2_crd.hp4) + Number(q2_crd.hp5) + Number(q2_crd.hp6) + Number(q2_crd.hp7) + Number(q2_crd.hp8) + Number(q2_crd.hp9) + Number(q2_crd.hp10);
+                let insert_to = document.querySelector("#grade11_sem1")
+                insertToTable(grade11_sem1, insert_to)
 
-                    
-                    // calculate grade
-                    console.log('index :>> ', index);
-                    q1_grade = calculateGrade(grade11_sem1[index], q1_crd)
-                    q2_grade = calculateGrade(grade11_sem1[index+1], q2_crd)
-                    
-                    console.log('q1_grade :>> ', q1_grade);
-                    console.log('q2_grade :>> ', q2_grade);
-
-                    tr = document.createElement('tr');
-                    tr.innerHTML = `
-                        <th scope="row">${data.subject_name}</th>
-                        <td>${q1_grade.final}</td>
-                        <td>${q2_grade.final}</td>
-                        <td>${(q1_grade.final + q2_grade.final) / 2}</td>
-                    `;
-                    grade11_sem1_insert_to.appendChild(tr);
-                })
-
-                // insert grade 11 sem 2
-                let grade11_sem2_insert_to = document.querySelector("#grade11_sem2");
-                grade11_sem2.forEach(async(data, index) => {
-
-                    if (data.quarter == 2){
-                        return;
-                    }
-                    // get classrecord detail quarter 1
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=1`);
-                    q1_crd = res.data;
-                    q1_crd.total_highest_written = Number(q1_crd.hw1) + Number(q1_crd.hw2) + Number(q1_crd.hw3) + Number(q1_crd.hw4) + Number(q1_crd.hw5) + Number(q1_crd.hw6) + Number(q1_crd.hw7) + Number(q1_crd.hw8) + Number(q1_crd.hw9) + Number(q1_crd.hw10);
-                    q1_crd.total_highest_performance = Number(q1_crd.hp1) + Number(q1_crd.hp2) + Number(q1_crd.hp3) + Number(q1_crd.hp4) + Number(q1_crd.hp5) + Number(q1_crd.hp6) + Number(q1_crd.hp7) + Number(q1_crd.hp8) + Number(q1_crd.hp9) + Number(q1_crd.hp10);
-                    
-                    // get classrecord detail quarter 2
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=2`);
-                    q2_crd = res.data;
-                    q2_crd.total_highest_written = Number(q2_crd.hw1) + Number(q2_crd.hw2) + Number(q2_crd.hw3) + Number(q2_crd.hw4) + Number(q2_crd.hw5) + Number(q2_crd.hw6) + Number(q2_crd.hw7) + Number(q2_crd.hw8) + Number(q2_crd.hw9) + Number(q2_crd.hw10);
-                    q2_crd.total_highest_performance = Number(q2_crd.hp1) + Number(q2_crd.hp2) + Number(q2_crd.hp3) + Number(q2_crd.hp4) + Number(q2_crd.hp5) + Number(q2_crd.hp6) + Number(q2_crd.hp7) + Number(q2_crd.hp8) + Number(q2_crd.hp9) + Number(q2_crd.hp10);
+                insert_to = document.querySelector("#grade11_sem2")
+                insertToTable(grade11_sem2, insert_to)
 
 
-                    // calculate grade
-                    console.log('index :>> ', index);
-                    q1_grade = calculateGrade(grade11_sem2[index], q1_crd)
-                    q2_grade = calculateGrade(grade11_sem2[index+1], q2_crd)
-                    
-                    console.log('q1_grade :>> ', q1_grade);
-                    console.log('q2_grade :>> ', q2_grade);
-
-                    tr = document.createElement('tr');
-                    tr.innerHTML = `
-                        <th scope="row">${data.subject_name}</th>
-                        <td>${q1_grade.final}</td>
-                        <td>${q2_grade.final}</td>
-                        <td>${(q1_grade.final + q2_grade.final) / 2}</td>
-                    `;
-                    grade11_sem2_insert_to.appendChild(tr);
-                })   
-
-
-
-                //  ------ grade 12 -------
+                // ------ grade 12 -------
 
                 // divide grade 12 data to 2 semester
                 let grade12_sem1 = [], grade12_sem2 = []
-
                 grade12.forEach(data => {
                     if (data.semester == 1) {
                         grade12_sem1.push(data)
@@ -257,81 +180,13 @@
                 console.log('grade12_sem1 :>> ', grade12_sem1);
                 console.log('grade12_sem2 :>> ', grade12_sem2);
 
-                // insert grade 12 sem 1
-                let grade12_sem1_insert_to = document.querySelector("#grade12_sem1");
-                grade12_sem1.forEach(async(data, index) => {
 
-                    if (data.quarter == 2){
-                        return;
-                    }
-                    // get classrecord detail quarter 1
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=1`);
-                    q1_crd = res.data;
-                    q1_crd.total_highest_written = Number(q1_crd.hw1) + Number(q1_crd.hw2) + Number(q1_crd.hw3) + Number(q1_crd.hw4) + Number(q1_crd.hw5) + Number(q1_crd.hw6) + Number(q1_crd.hw7) + Number(q1_crd.hw8) + Number(q1_crd.hw9) + Number(q1_crd.hw10);
-                    q1_crd.total_highest_performance = Number(q1_crd.hp1) + Number(q1_crd.hp2) + Number(q1_crd.hp3) + Number(q1_crd.hp4) + Number(q1_crd.hp5) + Number(q1_crd.hp6) + Number(q1_crd.hp7) + Number(q1_crd.hp8) + Number(q1_crd.hp9) + Number(q1_crd.hp10);
-                    
-                    // get classrecord detail quarter 2
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=2`);
-                    q2_crd = res.data;
-                    q2_crd.total_highest_written = Number(q2_crd.hw1) + Number(q2_crd.hw2) + Number(q2_crd.hw3) + Number(q2_crd.hw4) + Number(q2_crd.hw5) + Number(q2_crd.hw6) + Number(q2_crd.hw7) + Number(q2_crd.hw8) + Number(q2_crd.hw9) + Number(q2_crd.hw10);
-                    q2_crd.total_highest_performance = Number(q2_crd.hp1) + Number(q2_crd.hp2) + Number(q2_crd.hp3) + Number(q2_crd.hp4) + Number(q2_crd.hp5) + Number(q2_crd.hp6) + Number(q2_crd.hp7) + Number(q2_crd.hp8) + Number(q2_crd.hp9) + Number(q2_crd.hp10);
+                insert_to = document.querySelector("#grade12_sem1")
+                insertToTable(grade12_sem1, insert_to)
 
+                insert_to = document.querySelector("#grade12_sem2")
+                insertToTable(grade12_sem2, insert_to)
 
-                    // calculate grade
-                    console.log('index :>> ', index);
-                    q1_grade = calculateGrade(grade12_sem1[index], q1_crd)
-                    q2_grade = calculateGrade(grade12_sem1[index+1], q2_crd)
-                    
-                    console.log('q1_grade :>> ', q1_grade);
-                    console.log('q2_grade :>> ', q2_grade);
-
-                    tr = document.createElement('tr');
-                    tr.innerHTML = `
-                        <th scope="row">${data.subject_name}</th>
-                        <td>${q1_grade.final}</td>
-                        <td>${q2_grade.final}</td>
-                        <td>${(q1_grade.final + q2_grade.final) / 2}</td>
-                    `;
-                    grade12_sem1_insert_to.appendChild(tr);
-                })
-
-                // insert grade 12 sem 2
-                let grade12_sem2_insert_to = document.querySelector("#grade12_sem2");
-                grade12_sem2.forEach(async(data, index) => {
-
-                    if (data.quarter == 2){
-                        return;
-                    }
-                    // get classrecord detail quarter 1
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=1`);
-                    q1_crd = res.data;
-                    q1_crd.total_highest_written = Number(q1_crd.hw1) + Number(q1_crd.hw2) + Number(q1_crd.hw3) + Number(q1_crd.hw4) + Number(q1_crd.hw5) + Number(q1_crd.hw6) + Number(q1_crd.hw7) + Number(q1_crd.hw8) + Number(q1_crd.hw9) + Number(q1_crd.hw10);
-                    q1_crd.total_highest_performance = Number(q1_crd.hp1) + Number(q1_crd.hp2) + Number(q1_crd.hp3) + Number(q1_crd.hp4) + Number(q1_crd.hp5) + Number(q1_crd.hp6) + Number(q1_crd.hp7) + Number(q1_crd.hp8) + Number(q1_crd.hp9) + Number(q1_crd.hp10);
-                    
-                    // get classrecord detail quarter 2
-                    res = await axios.get(`http://localhost/teachers-toolkit-app/server/api/classrecord_detail/read_by_subject_assignment.php?subject_assignment_id=${data.subject_assignment_id}&quarter=2`);
-                    q2_crd = res.data;
-                    q2_crd.total_highest_written = Number(q2_crd.hw1) + Number(q2_crd.hw2) + Number(q2_crd.hw3) + Number(q2_crd.hw4) + Number(q2_crd.hw5) + Number(q2_crd.hw6) + Number(q2_crd.hw7) + Number(q2_crd.hw8) + Number(q2_crd.hw9) + Number(q2_crd.hw10);
-                    q2_crd.total_highest_performance = Number(q2_crd.hp1) + Number(q2_crd.hp2) + Number(q2_crd.hp3) + Number(q2_crd.hp4) + Number(q2_crd.hp5) + Number(q2_crd.hp6) + Number(q2_crd.hp7) + Number(q2_crd.hp8) + Number(q2_crd.hp9) + Number(q2_crd.hp10);
-
-
-                    // calculate grade
-                    console.log('index :>> ', index);
-                    q1_grade = calculateGrade(grade12_sem2[index], q1_crd)
-                    q2_grade = calculateGrade(grade12_sem2[index+1], q2_crd)
-                    
-                    console.log('q1_grade :>> ', q1_grade);
-                    console.log('q2_grade :>> ', q2_grade);
-
-                    tr = document.createElement('tr');
-                    tr.innerHTML = `
-                        <th scope="row">${data.subject_name}</th>
-                        <td>${q1_grade.final}</td>
-                        <td>${q2_grade.final}</td>
-                        <td>${(q1_grade.final + q2_grade.final) / 2}</td>
-                    `;
-                    grade12_sem2_insert_to.appendChild(tr);
-                })   
 
 
 
@@ -340,6 +195,23 @@
             }
         });
 
+        function insertToTable(data, insert_to){
+            let tr
+            data.forEach((elem, index) => {
+                if (elem.quarter == 2){
+                    return
+                }
+                tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <th scope="row">${data[index].subject_name}</th>
+                    <td>${data[index].final_grade}</td>
+                    <td>${data[index+1].final_grade}</td>
+                    <td>${(data[index].final_grade + data[index+1].final_grade) / 2}</td>
+                `;
+                insert_to.appendChild(tr);
+                
+            })
+        }
 
 
 
